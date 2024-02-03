801485edb2c48f6230a427588aaa936f1a9a08eb
Sam Colin <samantha.colin022@gmail.com>
2024-02-03 12:55:14 -0600

    fix(post): allow new account creation and fix username ID conflicts

--- models/userModel.js
+++ models/userModel.js
@@ -1,41 +1,36 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-const userSchema = new mongoose.Schema ({
-	firstName: { // should match name= in html form
-		type: String, 
-		required: true, 
-	},
-	lastName: { 
-		type: String, 
-		required: true, 
-	},
-    userName: { 
-		type: String, 
-		required: true, 
-		unique: true 
-	},
-    email: { 
-		type: String, 
-		required: true, 
-		unique: true 
-	},
-    password: { 
-		type: String, 
-		required: true 
-	},
-	confirmPassword: { 
-		type: String, 
-		required: true 
-	},
-    createdAt: {
-		type: Date, 
-		default: Date.now 
-	}
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String, 
+    required: true
+  },
+  lastName: { 
+    type: String, 
+    required: true
+  },
+  userName: { 
+    type: String, 
+    required: true, 
+    unique: true
+  },
+  email: { 
+    type: String, 
+    required: true,
+  },
+  password: { 
+    type: String, 
+    required: true 
+  },
+  createdAt: {
+    type: Date, 
+    default: Date.now 
+  }
 });
 
-userSchema.index({ userName: 1 }, { unique: true });
-userSchema.index({ email: 1 }, { unique: true });
-
 const User = mongoose.model('User', userSchema);
+
+// User.updateMany({userName: null}, { $unset : { name : 1 }})
 
 module.exports = User;
