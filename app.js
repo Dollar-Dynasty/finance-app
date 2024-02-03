fc51ba2b79d543ea1c4fab30da5858b1c491ec9d
Sam Colin <samantha.colin022@gmail.com>
2024-02-03 08:05:07 -0600

    feat(global): change view engine routes in app.js to EJS

--- app.js
+++ app.js
@@ -9,17 +9,31 @@ connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.set('view engine', 'pug');
+// makes EJS the template engine
+app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => { res.render('index'); });
+// Load HTML pages from 'views/pages' //
+
+app.get('/', (req, res) => { 
+  res.render('pages/index'); 
+});
+
+app.get('/register', (req, res) => { 
+  res.render('pages/register'); 
+});
+
+app.get('/login', (req, res) => { 
+  res.render('pages/login'); 
+});
+
 app.get('/:version', function(req, res) { res.send(req.params.version); });
 
-app.post('/', (req, res) => {
+app.post('/register', (req, res) => {
   const { firstName, lastName, userName, email, password, confirmPassword } = req.body;
   const hashedPassword = ""; // hash the password using bcrypt or something similar
   const newUser = new User({ firstName, lastName, userName, email, password, confirmPassword, });
   newUser.save();
-  res.redirect('/dashboard');
+  res.redirect('/home');
   res.send({ newUser });
 })
 
