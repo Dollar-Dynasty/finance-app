8e0856f7b088fbe625dc1c6f6e0697d04a520e03
Sam Colin <samantha.colin022@gmail.com>
2024-02-22 03:55:15 -0600

    chore: checkpoint

--- routes/budget-routes.js
+++
@@ -1,7 +0,0 @@
-// const express = require('express');
-// const apiRouter = express.Router();
-// const budgetController = require('../controller/budget-controller');
-
-// apiRouter.post('/budget-form', budgetController.createBudget);
-
-// module.exports = apiRouter;
--- routes/view-routes.js
+++ routes/view-routes.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const viewRouter = express.Router();
 
-// Landing
+
+/// --AUTHENICATION  ///
+viewRouter.get('/login', (req, res) => {res.render('pages/auth/login-page');});
+viewRouter.get('/register', (req, res) => {res.render('pages/auth/registration-page');});
+
+// --LANDING  ///
 viewRouter.get('/', (req, res) => {res.render('pages/index');});
-viewRouter.get('/login', (req, res) => {res.render('pages/landing-links/user-login');});
 
-// New User
-viewRouter.get('/register', (req, res) => {res.render('pages/new-user/account-registration');});
-viewRouter.get('/make-goal', (req, res) => {res.render('pages/new-user/first-goal');});
-viewRouter.get('/create-budget', (req, res) => {res.render('pages/new-user/first-budget');});
-viewRouter.get('/register', (req, res) => {res.render('pages/new-user/account-registration');});
+/// --DASHBOARDS  ///
+viewRouter.get('/dashboard', (req, res) => {res.render('pages/main-dashboard');});
+viewRouter.get('/budget-overview', (req, res) => {res.render('pages/budget/budget-dashboard');});
+viewRouter.get('/goals-overview', (req, res) => {res.render('pages/goal/goals-dashboard');});
 
-// Logged In User
-viewRouter.get('/dashboard', (req, res) => {res.render('pages/logged-in-user/user-dashboard');});
-viewRouter.get('/budget-overview', (req, res) => {res.render('pages/logged-in-user/budgets-overview');});
-viewRouter.get('/goals-overview', (req, res) => {res.render('pages/logged-in-user/goals-overview');});
+/// --BUDGET/GOAL CREATION   /// 
+viewRouter.get('/create-budget', (req, res) => {res.render('pages/budget/create-budget-page');});
+viewRouter.get('/make-goal', (req, res) => {res.render('pages/goal/create-goal-page');});
+
 
 module.exports = viewRouter;
